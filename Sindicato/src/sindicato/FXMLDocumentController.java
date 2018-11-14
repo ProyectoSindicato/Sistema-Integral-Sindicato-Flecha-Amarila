fa2528613fe3a5e3bc8d2545d6ddf2c3b69e1dde
JorgeGuani2 <jorge.guani@gmail.com>
2018-11-13 23:34:04 -0600

    Modficación de iconos y hovers en la interfaz principal

--- Sindicato/src/sindicato/FXMLDocumentController.java
+++ Sindicato/src/sindicato/FXMLDocumentController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -22,9 +23,11 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     
     @FXML
+    private Button btnButton1;
+    
+    @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        System.out.println("You clicked me!");        
     }
     
     @Override
