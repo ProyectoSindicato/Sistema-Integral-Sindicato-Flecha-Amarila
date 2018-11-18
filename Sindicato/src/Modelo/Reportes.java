ca32ff65c395d835b0409c81aab0443a43f26a8c
kohire <42823603+kohire@users.noreply.github.com>
2018-11-17 18:30:04 -0600

    ConexiónBD.
    Conexión a la base de datos.
    Recuerden cambiar la localización.

--- Sindicato/src/Modelo/Reportes.java
+++ Sindicato/src/Modelo/Reportes.java
@@ -1,14 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelo;
-
 /**
  *
  * @author Aideé Alvarez
  */
 public class Reportes {
+
+    private int Id;
+    private int IdConductor;
+    private int IdDirector;
+    private int IdDelegado;
+    private String tipo;
+    private String lugar;
+    private String fecha;
+    private String descripcion;
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public void setIdConductor(int IdConductor) {
+        this.IdConductor = IdConductor;
+    }
+
+    public void setIdDirector(int IdDirector) {
+        this.IdDirector = IdDirector;
+    }
+
+    public void setIdDelegado(int IdDelegado) {
+        this.IdDelegado = IdDelegado;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public int getIdConductor() {
+        return IdConductor;
+    }
+
+    public int getIdDirector() {
+        return IdDirector;
+    }
+
+    public int getIdDelegado() {
+        return IdDelegado;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    
+    
+    
     
 }
