282adbc9df591c594cba715a196cfd5e2841bbfb
IsaacPerezSegura <15240485@itleon.edu.mx>
2018-11-23 20:07:30 -0600

    Añadiendo campos fecha a incapacidades

--- Sindicato/src/controladores/IncapacidadesController.java
+++ Sindicato/src/controladores/IncapacidadesController.java
@@ -1,27 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.GridPane;
 
-/**
- * FXML Controller class
- *
- * @author Isaac Perez
- */
 public class IncapacidadesController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private GridPane datePickerPanel,datePickerPanel2;
+    @FXML 
+    private DatePicker date,date2;
+    @FXML
+    private Button registrar;
+    void datePicker()
+      {
+           date = new DatePicker();
+           datePickerPanel.add(date, 0, 0);
+           date2 = new DatePicker();
+           datePickerPanel2.add(date2, 0, 0);
+      }
+    @FXML
+    private void handleButtonAction(ActionEvent e){
+        if(e.getSource() == registrar){
+            System.out.println(date.getValue()); 
+            System.out.println(date2.getValue());
+        }
+    } 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        datePicker();
     }    
     
 }
