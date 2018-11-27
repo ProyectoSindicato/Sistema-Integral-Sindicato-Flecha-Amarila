8a9a4d79c6da6d40359ffe4feca9cac163bced05
kohire <42823603+kohire@users.noreply.github.com>
2018-11-26 21:51:08 -0600

    Reportes3.0
    Agregación de búsqueda por medio de ChangeListener al campo idConductor.

--- Sindicato/src/controladores/VistaReportesController.java
+++ Sindicato/src/controladores/VistaReportesController.java
@@ -24,6 +24,9 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javax.swing.JOptionPane;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -55,16 +58,18 @@ public class VistaReportesController implements Initializable {
     @FXML
     private TableColumn<Reportes, Integer> colID;
     @FXML
-    private TableColumn<Reportes, Integer> colConductor;
+    private TableColumn<Reportes, String> colConductor;
     @FXML
     ComboBox<String> comboBox = new ComboBox<String>();
+    @FXML
+    private Button btnAgregar, btnModificar, btnEliminar;
 
-    //pruebas con el datepicker.
     @FXML
     private GridPane DatePickerPanel;
     private final DatePicker date;
-
+    ManejadorEventos eventoFiltro;
     private String idUsuario; // Usuario que insertará o modificará.
+    private boolean filtroActivo; //Filtro del changeListener.
 
     ObservableList<String> boxOpciones
             = FXCollections.observableArrayList("General", "Inspección", "Tacografía",
@@ -75,38 +80,40 @@ public class VistaReportesController implements Initializable {
     public VistaReportesController() {
         conexionBD = new ConexionAccess();
         date = new DatePicker();
-        
+
     }
 
     void cargarElementos() {
         comboBox.getItems().addAll(boxOpciones);
         DatePickerPanel.add(date, 0, 0);
-      
+
     }
 
     /* SECCION FXMLLoader */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         this.idUsuario = ""; //prueba.
+        filtroActivo = false; //Nuestro filtro comienza en false.
         cargarElementos();
         llenarTablaBD();
+        eventoFiltro = new ManejadorEventos(); //Activamos el evento.
     }
 
     /* 
             Sección 1. Funcionamiento de botones.
      */
     // Función de agregar.
+    @FXML
     public void agregarReporte(ActionEvent e) {
         int confirmar;
         //  String idRep = txtReporte.getText();
         String idUsr = txtUsuario.getText();
-        String idUsr2 = "";
         String idCond = txtConductor.getText();
         String lugar = txtLugar.getText();
         LocalDate value = date.getValue();
         String combo = comboBox.getSelectionModel().getSelectedItem();
         String desc = txtDescripcion.getText();
-        
+
         if ((!(desc.equals(""))) && (combo != null)
                 && (!(idCond.equals(""))) && (!(lugar.equals(""))) && (value != null)) {
             confirmar = JOptionPane.showConfirmDialog(null, "¿Desea ingresar un reporte?", "Confirmar agregar reporte.",
@@ -115,16 +122,14 @@ public class VistaReportesController implements Initializable {
             if (confirmar == 0) {
                 try {
                     Reportes bean = new Reportes();
-                    bean.setIdConductor(Integer.parseInt(idCond));
-                    bean.setIdDirector(Integer.parseInt(idUsr));
-                  //  bean.setIdDelegado(Integer.parseInt(idUsr2));
+                    bean.setIdConductor(idCond);
+                    bean.setIdEmpleado(idUsr);
                     bean.setTipo(combo);
                     bean.setLugar(lugar);
                     bean.setFecha(String.valueOf(value));
-                   bean.setDescripcion(desc);
-                    int i = Integer.parseInt( txtUsuario.getText());
-                    if (bean.insertarReporte(i) == true) {
-                        ActualizaRefresca();
+                    bean.setDescripcion(desc);
+                    if (bean.insertarReporte(idUsr) == true) {
+                        insertarYRefrescar(); //Aquí se cambia por el insertar(que contiene el rs).
                         JOptionPane.showMessageDialog(null, "Se ha agregado el reporte.");
                     } else {
                         JOptionPane.showMessageDialog(null, "Error al agregar un reporte.");
@@ -132,7 +137,7 @@ public class VistaReportesController implements Initializable {
 
                 } catch (HeadlessException | NumberFormatException ex) {
                     JOptionPane.showMessageDialog(null, "Error al ingresar. Checar datos.");
-                    System.out.println("Aquí está el error: " +ex.getLocalizedMessage());
+                    System.out.println("Aquí está el error: " + ex.getLocalizedMessage());
                 }
             }
         } else {
@@ -143,10 +148,12 @@ public class VistaReportesController implements Initializable {
     } //botón.
 
     // Función de modificar.
+    @FXML
     public void modificarReporte(ActionEvent e) {
         int confirmar;
         Reportes modificar = tablaReporte.getSelectionModel().getSelectedItem();
-
+        int id = Integer.parseInt(txtReporte.getText());
+        String idUsr = txtUsuario.getText();
         String idConductor = txtConductor.getText();
         String tipo = comboBox.getValue();
         String lugar = txtLugar.getText();
@@ -164,15 +171,16 @@ public class VistaReportesController implements Initializable {
                 if (confirmar == 0) {
                     try {
                         Reportes m = new Reportes();
-
-                        m.setIdConductor(Integer.parseInt(idConductor));
+                        m.setId(id);
+                        m.setIdConductor(idConductor);
                         m.setTipo(tipo);
                         m.setLugar(lugar);
                         m.setFecha(String.valueOf(value));
                         m.setDescripcion(desc);
-                        if (m.modificarReporte() == true) {
+                        if (m.modificarReporte(idUsr) == true) {
+                            insertarYRefrescar();
                             JOptionPane.showMessageDialog(null, "Modificación exitosa.");
-                            ActualizaRefresca();
+
                         } else {
                             JOptionPane.showMessageDialog(null, "Error al modificar reporte.");
                         }
@@ -190,6 +198,7 @@ public class VistaReportesController implements Initializable {
     }
 
     // Funcion de eliminar.
+    @FXML
     public void eliminarReporte(ActionEvent e) {
         int confirmar;
         Reportes borrar = tablaReporte.getSelectionModel().getSelectedItem();
@@ -206,7 +215,7 @@ public class VistaReportesController implements Initializable {
                     eliminar.setId(idReporte);
 
                     if (eliminar.eliminarReporte(idReporte)) {
-                        ActualizaRefresca();
+                        insertarYRefrescar();
                         JOptionPane.showMessageDialog(null, "Reporte eliminado exitosamente.");
                     } else {
                         JOptionPane.showMessageDialog(null, "Error al eliminar reporte. ");
@@ -221,21 +230,15 @@ public class VistaReportesController implements Initializable {
 
     }
 
-    // Buscar reporte.
-    public void buscarReporte() {
-
-    }
-
     /* 
         Sección 2. Manipulación de tableview y tablecolumn.
      */
     @FXML
-    private void tablaReportesAction (MouseEvent e)
-    {
+    private void tablaReportesAction(MouseEvent e) {
         mostrarFilaEnCampos();
     }
-    
-    public void llenarTablaBD() //Llena la tabla principal.
+
+    public void llenarTablaBD() //Llena la tabla principal al momento en que se construye el fxml.
     {
         ResultSet rs;
         String sql = "SELECT Id, IdConductor, Tipo, Fecha, Descripcion, Lugar FROM Reportes";
@@ -261,13 +264,61 @@ public class VistaReportesController implements Initializable {
                 String fecha = String.valueOf(rs.getDate(4));
                 String desc = String.valueOf(fila[4]);
                 String lugar = String.valueOf(fila[5]);
-                Reportes rep = new Reportes(Integer.parseInt(id), Integer.parseInt(idCond), 
-                                            tipo, fecha, desc,lugar);
-                 colocarDatosColumna();
-                 tablaReporte.getItems().add(rep);    
+                Reportes rep = new Reportes(Integer.parseInt(id), idCond, tipo, lugar, fecha, desc);
+                colocarDatosColumna();
+
+                tablaReporte.getItems().add(rep);
             }
         } catch (SQLException e) {
             System.out.println("Error al rellenar la tabla." + e.getMessage());
+        }
+    }
+
+    /* ¿Qué sucede aquí? Bueno, hay dos opciones:
+        1.- Buscas por la acción del botón.
+        2.- Todo normal como si leyeras la base de datos.
+     */
+    public void actualizarTablaBD(ResultSet result) //Llenar la tabla con un result set.
+    {
+        ResultSet rs;
+        try {
+            if (filtroActivo) { //Si el filtro está activado, quiere decir que el txtField está en escucha y se sustituye el result set por ese.
+                rs = result;
+            } else { //En caso contrario, solamente volvemos a ejecutar la sentencia normal de traer todo a la tabla.
+                String sql = "SELECT Id, IdConductor, Tipo, Fecha, Descripcion, Lugar FROM Reportes";
+                rs = conexionBD.ejecutarSQLSelect(sql);
+            }
+
+            try { //Todo esto es lo mismo a como si llenaras la tabla normal. Lo único que cambia es lo de arriba.
+                conexionBD.conectar();
+                metadata = rs.getMetaData();
+                int cols = metadata.getColumnCount();
+                while (rs.next()) {
+                    Object[] fila = new Object[cols];
+                    for (int i = 0; i < cols; i++) {
+                        if (rs.getObject(i + 1) == null) {
+                            fila[i] = "";
+
+                        } else {
+                            fila[i] = rs.getObject(i + 1);
+                        }
+                    }
+                    String id = String.valueOf(fila[0]);
+                    String idCond = String.valueOf(fila[1]);
+                    String tipo = (String) fila[2];
+                    String fecha = String.valueOf(rs.getDate(4));
+                    String desc = String.valueOf(fila[4]);
+                    String lugar = String.valueOf(fila[5]);
+                    Reportes rep = new Reportes(Integer.parseInt(id), idCond, tipo, lugar, fecha, desc);
+                    colocarDatosColumna();
+                    tablaReporte.getItems().add(rep);
+                }
+            } catch (SQLException e) {
+                System.out.println("Error al filtrar la tabla." + e.getMessage());
+            }
+
+        } catch (Exception e) {
+
         }
     }
 
@@ -281,9 +332,29 @@ public class VistaReportesController implements Initializable {
     }
 
     void ActualizaRefresca() {
+        ResultSet r = null;
         LimpiarTabla();
-        llenarTablaBD();
+        actualizarTablaBD(r);
         LimpiarCampos();
+    }
+
+    public void insertarYRefrescar() { //Este es importante. Si utilizas el otro, no va a funcionar la acción del botón.
+        LimpiarTabla();
+        ResultSet r = null; //Se regresa un nulo porque no lo estamos usando al momento de insertar o modificar.
+        actualizarTablaBD(r);
+        LimpiarCampos();
+    }
+
+    public void eliminarYActualizar() { //Intenta eliminarlo. Es por precaución por el resultset.
+        try {
+            LimpiarCampos();
+            LimpiarTabla();
+            ResultSet r = null;
+            actualizarTablaBD(r);
+            JOptionPane.showMessageDialog(null, "Se elimino el  con exito", "Eliminar", JOptionPane.PLAIN_MESSAGE);
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(null, "No se a seleccionado ningun Profesor", "Eliminar", JOptionPane.PLAIN_MESSAGE);
+        }
     }
 
     void LimpiarCampos() {
@@ -293,7 +364,7 @@ public class VistaReportesController implements Initializable {
         txtLugar.clear();
         txtDescripcion.clear();
         comboBox.setValue(" ");
-      
+
     }
 
     void LimpiarTabla() {
@@ -309,7 +380,7 @@ public class VistaReportesController implements Initializable {
             LimpiarCampos();
         } else {
             int IdReporte = reportes.getId();
-            int IdConductor = reportes.getIdConductor(); //Este será idEmpleado.
+            String IdConductor = reportes.getIdConductor();
             String lugar = reportes.getLugar();
             String fecha = reportes.getFecha();
             String descripcion = reportes.getDescripcion();
@@ -322,5 +393,57 @@ public class VistaReportesController implements Initializable {
             txtDescripcion.setText(descripcion);
         }
     }
-    
+
+    void manejadorFiltro() {
+        if (filtroActivo) { //Si es un true, obtenemos el idConductor para que haga la búsqueda.
+            String idConductor;
+            idConductor = txtConductor.getText();
+            leerFiltro(idConductor); //Este filtro es el que está más abajo.
+        }
+    }
+
+    @FXML
+    private void handleFiltro() { //Acción del botón.
+        filtroActivo = !filtroActivo; //Si el filtro es true (osea, se le dio el clic).
+
+        if (filtroActivo) { //Deshabilitamos todo y solamente dejamos al txtfield que queremos que nos escuche.
+            btnAgregar.setDisable(true);
+            btnEliminar.setDisable(true);
+            btnModificar.setDisable(true);
+
+            txtConductor.textProperty().addListener(eventoFiltro);
+            txtDescripcion.setDisable(true);
+            txtLugar.setDisable(true);
+            txtDescripcion.setDisable(true);
+            comboBox.setDisable(true);
+            date.setDisable(true);
+
+        } else { // Sino, se vuelve un false y activamos todo a como estaba antes.
+            btnAgregar.setDisable(false);
+            btnEliminar.setDisable(false);
+            btnModificar.setDisable(false);
+            txtConductor.textProperty().removeListener(eventoFiltro);
+            txtDescripcion.setDisable(false);
+            txtLugar.setDisable(false);
+            txtDescripcion.setDisable(false);
+            comboBox.setDisable(false);
+            date.setDisable(false);
+        }
+    }
+
+    // Este filtro limpia toda la tabla y busca por medio del resultset toda la información que yo quiero ver.
+    private void leerFiltro(String idConductor) {
+        Reportes filtro = new Reportes();
+        LimpiarTabla();
+        actualizarTablaBD(filtro.filtrarReporte(idConductor)); //Checa en Modelo -> Reportes.
+        System.out.println("Entré al filtro de controller.");
+    }
+
+    class ManejadorEventos implements ChangeListener { //Manejador de eventos para el changelistener.
+
+        @Override
+        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            manejadorFiltro(); //Aquí metes el manejadorFiltro, ya que contiene el leerFiltro(con su resultset).
+        }
+    }
 }
